4878348c2890ff1a4734de5f7650520c76132e7d
Justin Russell <Justin@Justins-MacBook.local>
2020-03-04 14:06:33 -0800

    styling

--- expat/src/components/Header.js
+++ expat/src/components/Header.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../App.css";
+import styled from 'styled-components';
+
+
+const NavBar = styled.nav`
+  display: flex;
+  width: 20%;
+  border: 1px solid #1978a5;
+  justify-content: space-between;
+`;
+
 
 export default function Header() {
-
+    
+    const signOut = () => {
+      window.localStorage.removeItem("token");
+    };
     
 
     return (
-        <div className="header">
-            <Link to="/" className="title">Expat App</Link>
-            <nav className="nav-links">
-                <Link className="nav-link" to ="/login">Login</Link>
-               
-            </nav>
-        </div>
-    )
+      <div className="header">
+        <NavBar>
+          <Link className="nav-links" to="/dashboard">
+            Dashboard
+          </Link>
+          <Link className="nav-links" to="/profile">
+            Profile
+          </Link>
+          <Link className="nav-links" onClick={signOut}>
+            Sign Out
+          </Link>
+        </NavBar>
+      </div>
+    );
 }
